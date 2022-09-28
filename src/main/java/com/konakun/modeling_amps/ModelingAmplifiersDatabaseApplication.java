479566c4b32a0e4bbc9d15de43e83e594369f88b
WIN-SIS-HHERNAN\Arturo <dragonzerker@live.com>
2022-09-28 09:00:08 -0600

    feature: Spring Base Inserted

---
+++ src/main/java/com/konakun/modeling_amps/ModelingAmplifiersDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.konakun.modeling_amps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelingAmplifiersDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelingAmplifiersDatabaseApplication.class, args);
+	}
+
+}
