86a66942df4e2fb858bf5ebe1001583e189568cd
WIN-SIS-HHERNAN\Arturo <dragonzerker@live.com>
2022-11-01 15:28:21 -0700

    chore: Added Modify Endpoint, changed routes to conform to REST Model, replaced values with constants in Queries.

--- src/main/java/com/konakun/modeling_amps/brand/BrandController.java
+++ src/main/java/com/konakun/modeling_amps/brand/BrandController.java
@@ -1,12 +1,14 @@
 package com.konakun.modeling_amps.brand;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
+
+import static javax.servlet.http.HttpServletResponse.*;
 
 @RestController
 @RequestMapping("/brands")
@@ -14,41 +16,94 @@ public class BrandController {
     @Autowired
     BrandRepository brandRepo;
 
-    @PostMapping("/add")
-    @ResponseBody
-    public Brand saveBrand(@RequestBody Brand brand) {
-        Brand saved_brand = brandRepo.save(brand);
-        return saved_brand;
-    }
-
-    @DeleteMapping("/{primary_key}/delete")
-    @ResponseBody
-    public Brand deleteBrand(@PathVariable Integer primary_key, HttpServletResponse response){
-        return null;
-    }
-
-    @GetMapping("/find/{primary_key}")
-    @ResponseBody
-    public Optional<Brand> getBrand(@PathVariable Integer primary_key, HttpServletResponse response){
-        Optional<Brand> brand_item = brandRepo.findById(primary_key);
-        System.out.println(brand_item);
-        if(brand_item.isEmpty()){
-            response.setStatus(400);
-            return null;
-        }
-        response.setStatus(200);
-        return brand_item;
-    }
-
     @GetMapping("/all")
     @ResponseBody
-    public List<Brand> getAllBrands(){
-        return brandRepo.findAll();
+    public List<Brand> getAllBrands(HttpServletResponse response){
+        List<Brand> brands =  brandRepo.findAll();
+        if(brands.isEmpty()){
+            response.setStatus(SC_BAD_REQUEST);
+            return null;
+        }
+        response.setStatus(SC_OK);
+        return brands;
     }
 
     @GetMapping("/available")
     @ResponseBody
-    public List<Brand> getAvailableBrands(){
-        return brandRepo.findActivated();
+    public List<Brand> getAvailableBrands(HttpServletResponse response){
+        List<Brand> activated_brands = brandRepo.findActivatedBrands();
+        if(activated_brands.isEmpty()){
+            response.setStatus(SC_BAD_REQUEST);
+            return null;
+        }
+        response.setStatus(SC_OK);
+        return activated_brands;
+    }
+
+    @PostMapping("/add")
+    @ResponseBody
+    public Brand saveBrand(@RequestBody Brand brand, HttpServletResponse response) {
+        Brand saved_brand = brandRepo.save(brand);
+
+        if(saved_brand != null){
+            response.setStatus(SC_BAD_REQUEST);
+            return null;
+        }
+        response.setStatus(SC_CREATED);
+        return saved_brand;
+    }
+
+    @GetMapping("/{primary_key}")
+    @ResponseBody
+    public Optional<Brand> getBrand(@PathVariable Integer primary_key, HttpServletResponse response){
+        Optional<Brand> brand_item = brandRepo.findById(primary_key);
+        if(brand_item.isEmpty()){
+            response.setStatus(SC_BAD_REQUEST);
+            return null;
+        }
+        response.setStatus(SC_OK);
+        return brand_item;
+    }
+
+    @PutMapping("/{primary_key}")
+    @ResponseBody
+    public Optional<Brand> modifyBrand(@PathVariable Integer primary_key, @RequestBody Brand brand,
+                                       HttpServletResponse response){
+        Integer modify_brand = brandRepo.modifyBrand(primary_key, brand);
+
+        if(modify_brand < 1){
+            response.setStatus(SC_BAD_REQUEST);
+            return null;
+        }
+
+        Optional<Brand> modified_brand = brandRepo.findById(primary_key);
+
+        if(modify_brand < 1){
+            response.setStatus(SC_BAD_REQUEST);
+            return null;
+        }
+        response.setStatus(SC_OK);
+
+        return modified_brand;
+    }
+
+    @DeleteMapping("/{primary_key}")
+    @ResponseBody
+    public Optional<Brand> deleteBrand(@PathVariable Integer primary_key, HttpServletResponse response){
+        Integer deactivate_brand = brandRepo.disableBrand(primary_key);
+        if(deactivate_brand < 1){
+            response.setStatus(SC_BAD_REQUEST);
+            return null;
+        }
+
+        Optional<Brand> deactivated_brand = brandRepo.findById(primary_key);
+
+        if(deactivated_brand.isEmpty()){
+            response.setStatus(SC_BAD_REQUEST);
+            return null;
+
+        }
+        response.setStatus(SC_OK);
+        return deactivated_brand;
     }
 }
--- src/main/java/com/konakun/modeling_amps/brand/BrandRepository.java
+++ src/main/java/com/konakun/modeling_amps/brand/BrandRepository.java
@@ -1,13 +1,33 @@
 package com.konakun.modeling_amps.brand;
 
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
+import static com.konakun.modeling_amps.utilities.Constants.ACTIVE;
+import static com.konakun.modeling_amps.utilities.Constants.INACTIVE;
+
+@Scope("singleton")
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
 
-    @Query(value = "SELECT * FROM BRANDS WHERE ACTIVE = TRUE", nativeQuery = true)
-    List<Brand> findActivated();
+    @Query("SELECT b from Brand b WHERE b.active = " + ACTIVE)
+    List<Brand> findActivatedBrands();
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE Brand b SET b.active = " + INACTIVE + " WHERE b.pk_brand = :primary_key")
+    Integer disableBrand(@Param("primary_key") Integer primary_key);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Brand b SET b.name = :#{#brand.name}, b.active = " + ACTIVE
+            + " WHERE b.pk_brand = :primary_key")
+    Integer modifyBrand(@Param("primary_key") Integer primary_key, @Param("brand") Brand brand);
 }
