294182e2537d754f02bc68d8a595907ddcb97d8c
Arturo Cazares <31503959+konakun@users.noreply.github.com>
2022-11-04 12:27:16 -0700

    Revert "Chore/DeletePower"

--- src/main/java/com/konakun/modeling_amps/brand/BrandForm.java
+++
@@ -1,6 +0,0 @@
-package com.konakun.modeling_amps.brand;
-
-public class BrandForm {
-    private String name;
-    private Boolean activate;
-}
--- src/main/java/com/konakun/modeling_amps/utilities/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.konakun.modeling_amps.utilities;
-
-public final class Constants {
-    private Constants(){}
-
-    public static final String ACTIVE = "true";
-    public static final String INACTIVE = "false";
-
-}
